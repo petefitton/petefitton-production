f05bd9ce79e09f03fe28ab3b96007ea37781a6a6
Pete Fitton <petefitton@gmail.com>
2025-01-18 13:10:38 +0100

    Work on styling for Music Page and refactor MusicPage.js for better reuse of album cover containers

--- src/Pages/MusicPage.js
+++ src/Pages/MusicPage.js
@@ -1,22 +1,27 @@
 import React from 'react';
-// import './MusicPage.css';
-import albumCover from './The_Starting_of_Something_New.jpg';
+import './MusicPage.css';
+import tSOSNAlbumCover from './The_Starting_of_Something_New.jpg';
 
 function MusicPage() {
+  let releases = [{title: "The Starting of Something New", albumCoverImg: tSOSNAlbumCover, releaseDate: "February 5, 2025"}];
+
   return (
     <div className="musicpage-container pt-100">
       {/* <div className="content-container"> */}
         {/* <h1 className="band-name">Pete Fitton</h1> */}
         {/* <p className="band-description">A playful and enigmatic indie band.</p> */}
       {/* </div> */}
-      <div className="album-cover-container">
-        <img src={albumCover} alt="Latest Album Cover" className="album-cover-image" />
-        <div className="album-cover-info">
-          <h2>Latest Album Name</h2>
-          <p>Release Date: January 1, 2023</p>
-          <button>Listen Now</button>
+      {releases.map(release => (
+        <div className="album-cover-container album-cover-container-dark-blue">
+          <img src={release.albumCoverImg} alt="Latest Album Cover" className="album-cover-image" />
+          <div className="album-cover-info">
+            <h2>{release.title}</h2>
+            <p className="release-text-upper">Released on</p>
+            <p className="release-text-lower">{release.releaseDate}</p>
+            <button>Listen Now</button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
