dbeed97aedf936d94d403171b732c83d7cdd1ad3
Pete Fitton <petefitton@gmail.com>
2025-01-20 18:47:19 +0100

    Make Home Page video source change according to screen size

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
 function HomePage() {
-  let PUBLIC_URL = process.env.PUBLIC_URL;
+  const PUBLIC_URL = process.env.PUBLIC_URL;
+  const [isMobile, setIsMobile] = useState(null);
+  const handleResize = () => {
+    setIsMobile(window.matchMedia("(min-width: 768px)").matches);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  let videoSrc = isMobile ? `${PUBLIC_URL}/videos/TSOSN-mobile.mp4` : `${PUBLIC_URL}/videos/TSOSN-desktop.mp4`;
 
   return (
     <div className="homepage-container">
       <div className="background-animation">
-        {/* <video src={`${PUBLIC_URL}/videos/TSOSN-desktop.mp4`} alt="Latest Album Cover" className="hero-image" type="video/mp4" autoPlay muted loop /> */}
-        <video src={`${PUBLIC_URL}/videos/TSOSN-mobile.mp4`} alt="Latest Album Cover" className="hero-image" type="video/mp4" autoPlay muted loop />
+        <video src={videoSrc} alt="Latest Album Cover" className="hero-image" type="video/mp4" autoPlay muted loop />
       </div>
     </div>
   );
