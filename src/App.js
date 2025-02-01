faefa17e3005a24ecede48b3c0db65ce141cd743
Pete Fitton <petefitton@gmail.com>
2025-02-01 20:45:00 +0530

    Create Footer, clean up styling, remove unnecessary files and code

--- src/App.js
+++ src/App.js
@@ -2,30 +2,28 @@ import './App.css';
 import HomePage from './Pages/HomePage';
 import MusicPage from './Pages/MusicPage';
 // import StorePage from './Pages/StorePage';
-// import MiscPage from './Pages/MiscPage';
-// import AboutPage from './Pages/AboutPage';
 import ContactPage from './Pages/ContactPage';
+import Footer from './Pages/Footer';
 import NoPage from './Pages/NoPage';
 import Navbar from './Pages/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// TODO: add sticky footer for socials
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route index element={<HomePage />} />
-          <Route path="music" element={<MusicPage />} />
-          {/* <Route path="store" element={<StorePage />} /> */}
-          {/* <Route path="misc" element={<MiscPage />} /> */}
-          {/* <Route path="about" element={<AboutPage />} /> */}
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route index element={<HomePage />} />
+            <Route path="music" element={<MusicPage />} />
+            {/* <Route path="store" element={<StorePage />} /> */}
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </main>
+        <Footer />
       </BrowserRouter>
     </div>
   );
