ee3bad9061390e02c920c831efa09ed16b56643a
Pete Fitton <petefitton@gmail.com>
2025-02-02 20:41:24 +0530

    Reconfigure content on AboutPage and delete unused StorePage

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -23,9 +23,6 @@ function AboutPage() {
               <p>Reflective, inquisitive, and a little bit heady.  Pete Fitton is a 34 year-old multi-instrumentalist songwriter who has dived deep into meditation in recent years.  He has been involved in music from a young age in all kinds of musical contexts, such as musical theater, school band, orchestra, jazz, choir, barbershop quartet, medieval madrigals, and more.</p>
               <p>His upcoming double-EP "Sunset|Sunrise" is driven by vocals with acoustic guitar and contains other myriad key, string, synth, and percussion elements that complement that singer-songwriter core which ultimately results in a sound that is best described as indie acoustic.  In this work, his music takes one on a journey through despair and negativity towards love, optimism, and acceptance.<br></br>“The Starting of Something New” is the first single from this release.</p>
             </div>
-            <h1 className="mt-40">Contact</h1>
-            <p>petefittonmusic@gmail.com</p>
-            <h1 className="mt-40">Photos</h1>
             <div className="aboutpage-image-container">
               <img src={imageUrls[0]} alt="Pete Fitton" className="about-page-photo" />
               <img src={imageUrls[1]} alt="Pete Fitton" className="about-page-photo" />
@@ -33,6 +30,8 @@ function AboutPage() {
               <img src={imageUrls[3]} alt="Pete Fitton" className="about-page-photo" />
               <img src={imageUrls[4]} alt="Pete Fitton" className="about-page-photo" />
             </div>
+            <h1 className="mt-40">Contact</h1>
+            <p className="email">petefittonmusic@gmail.com</p>
           </div>
         </div>
       :
