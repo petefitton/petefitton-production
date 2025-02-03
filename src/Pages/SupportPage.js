b0e7ff3d51165e341f4b3747936929004c3c11a1
Pete Fitton <petefitton@gmail.com>
2025-02-03 14:51:50 +0530

    Add anchor tags for SupportPage and adjust styling

--- src/Pages/SupportPage.js
+++ src/Pages/SupportPage.js
@@ -16,9 +16,11 @@ function SupportPage() {
         <div className="supportpage-container pt-60">
           <div className="content-container">
             <h1>Buy me a coffee!</h1>
-            <p className="support-text">You can support me by buying my music on <a href="https://petefitton.bandcamp.com/" aria-label="Bandcamp" target="_blank" rel="noreferrer" className="bandcamp-anchor">my Bandcamp page</a>, and you can also tip me directly via the QR codes below. Most independent musicians make most of their money from album sales, merch, and tips, so any support beyond streaming is much appreciated, even sharing my music with others is helpful! :)<br></br>Thank you!</p>
+            <p className="support-text">You can support me by buying my music on <a href="https://petefitton.bandcamp.com/" aria-label="Bandcamp" target="_blank" rel="noreferrer" className="supportpage-anchor">my Bandcamp page</a>, and you can also tip me directly via Wise or Venmo below. Most independent musicians make most of their money from album sales, merch, and tips, so any support beyond streaming is much appreciated. Even sharing my music with others is helpful! :)<br></br>Thank you!</p>
             <div className="qr-code-container mt-40">
+                <a href="https://wise.com/pay/me/petehamiltonf" aria-label="Wise" target="_blank" rel="noreferrer" className="supportpage-anchor">Pete Fitton's Wise</a>
                 <img src={imageUrls[1]} alt="Pete Fitton's Wisetag" className="qr-code"></img>
+                <a href="https://venmo.com/Pete-Fitton" aria-label="Venmo" target="_blank" rel="noreferrer" className="supportpage-anchor">Pete Fitton's Venmo</a>
                 <img src={imageUrls[0]} alt="Pete Fitton's Venmo QR Code" className="qr-code"></img>
             </div>
           </div>
