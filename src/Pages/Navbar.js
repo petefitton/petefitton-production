02b36165fd5cac1587c9df693c4ee2ef72474143
Pete Fitton <petefitton@gmail.com>
2025-09-30 12:46:27 -0600

    Add semantic nav element to Navbar.js

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -5,28 +5,30 @@ import './Navbar.css';
 function Navbar() {
   return (
     <header className="navbar">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/music" className="nav-link">
-            Music
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/support" className="nav-link">
-            Support
-          </Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/music" className="nav-link">
+              Music
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/support" className="nav-link">
+              Support
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
