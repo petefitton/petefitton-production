12b366b6c3062ec58a3767140dc51fd11486cc51
Pete Fitton <petefitton@gmail.com>
2025-09-30 12:19:39 -0600

    Update footer to use "li" elements

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -7,11 +7,11 @@ function Footer() {
   return (
     <footer className="footer">
       <ul className="footer-list">
-        <a href="https://petefitton.bandcamp.com/" aria-label="Bandcamp" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faBandcamp} /></a>
-        <a href="https://open.spotify.com/artist/2kOaq98e4BfVwBnEp3PRbz" aria-label="Spotify" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faSpotify} /></a>
-        <a href="https://instagram.com/PeteFittonMusic" aria-label="Instagram" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faInstagram} /></a>
-        <a href="https://tiktok.com/@PeteFittonMusic" aria-label="TikTok" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faTiktok} /></a>
-        <a href="https://youtube.com/@PeteFittonMusic" aria-label="YouTube" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faYoutube} /></a>
+        <li><a href="https://petefitton.bandcamp.com/" aria-label="Bandcamp" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faBandcamp} /></a></li>
+        <li><a href="https://open.spotify.com/artist/2kOaq98e4BfVwBnEp3PRbz" aria-label="Spotify" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faSpotify} /></a></li>
+        <li><a href="https://instagram.com/PeteFittonMusic" aria-label="Instagram" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faInstagram} /></a></li>
+        <li><a href="https://tiktok.com/@PeteFittonMusic" aria-label="TikTok" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faTiktok} /></a></li>
+        <li><a href="https://youtube.com/@PeteFittonMusic" aria-label="YouTube" target="_blank" rel="noreferrer"><FontAwesomeIcon className="fa" icon={faYoutube} /></a></li>
       </ul>
     </footer>
   );
